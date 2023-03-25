ad97b8fdebcec979ac497db0e344e422020364e1
Babak <jahanibabak80@gmail.com>
2023-03-25 21:57:02 +0430

    change defualt base url

--- src/App/index.js
+++ src/App/index.js
@@ -2,9 +2,7 @@ import axios from "axios";
 import React from "react";
 
 const App = () => {
-  axios
-    .get("http://localhost:3001/employees")
-    .then((res) => console.log(res.data));
+  axios.get("/employees").then((res) => console.log(res.data));
   return (
     <div>
       <div>App</div>

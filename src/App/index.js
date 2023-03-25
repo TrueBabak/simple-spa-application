dceac52e417ca2744bfa7624e22cf011b6f835ff
Babak <jahanibabak80@gmail.com>
2023-03-25 21:55:01 +0430

    database config (json-server & axios)

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,10 @@
+import axios from "axios";
 import React from "react";
 
 const App = () => {
+  axios
+    .get("http://localhost:3001/employees")
+    .then((res) => console.log(res.data));
   return (
     <div>
       <div>App</div>

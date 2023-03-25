ba921eed230b6e066efc94a5996ae37bfaa444eb
Babak <jahanibabak80@gmail.com>
2023-03-25 23:28:52 +0430

    import context api in project

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import axios from "axios";
+import ContextsProvider from "./Components/Context";
 axios.defaults.baseURL = "http://localhost:3001/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextsProvider>
+      <App />
+    </ContextsProvider>
   </React.StrictMode>
 );
 // package we need in project => ..

93c554be269afbfa9b83e543431c692299638b13
Babak <jahanibabak80@gmail.com>
2023-03-25 22:01:27 +0430

    change db data

--- src/App/index.js
+++ src/App/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React from "react";
 
 const App = () => {
-  axios.get("/employees").then((res) => console.log(res.data));
+  axios.get("/contact").then((res) => console.log(res.data));
   return (
     <div>
       <div>App</div>
